6280065d1af342505ac2f4793f1840b3b2b90739
Gayathry Gopan <gayathrygopan714@gmail.com>
2025-11-20 20:38:17 +0530

    Reduced LIME samples to fix Streamlit crash

--- lime_explain.py
+++ lime_explain.py
@@ -1,36 +1,35 @@
 import numpy as np
 from lime import lime_image
 from skimage.segmentation import mark_boundaries
-
+import cv2
 
 def explain_with_lime(model, img_rgb):
-    """
-    Takes the original RGB image (H,W,3) and returns a LIME visualization.
-    """
 
-    img_float = img_rgb.astype("float32") / 255.0
+    # Resize image smaller ONLY for LIME (reduces processing cost)
+    img_small = cv2.resize(img_rgb, (128, 128))
 
+    img_float = img_small.astype("float32") / 255.0
     explainer = lime_image.LimeImageExplainer()
 
     def predict_fn(images):
         images = np.array(images)
-        return model.predict(images)
+        return model.predict(images, verbose=0)
 
     explanation = explainer.explain_instance(
         img_float,
         classifier_fn=predict_fn,
         top_labels=1,
         hide_color=0,
-        num_samples=1000
+        num_samples=120,   # From 1000 → 120 (Cloud safe)
     )
 
     top_label = explanation.top_labels[0]
 
     temp, mask = explanation.get_image_and_mask(
         top_label,
-        positive_only=False,
+        positive_only=True,
         hide_rest=False,
-        num_features=10,
+        num_features=5,  # From 10 → 5
         min_weight=0.0
     )
 
